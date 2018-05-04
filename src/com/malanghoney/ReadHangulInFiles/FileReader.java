6591e0d05d6d9e30675ba578933853f037921268
jayt.gonnichiwa <jayt.jhjeong@gmail.com>
2018-05-04 17:37:10 +0900

    Main 클래스에서 printHangulCode() 분리 (to FIleReader.java)

---
+++ src/com/malanghoney/ReadHangulInFiles/FileReader.java
@@ -0,0 +1,42 @@
+package com.malanghoney.ReadHangulInFiles;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+
+class FileReader {
+	private static HangulDetector hd = HangulDetector.getInstance();
+
+	FileReader(){}
+
+	void printHangulCode() {
+		Charset inputCharset;
+
+		String filePath = "C:\\dev-Source\\cafe24FTP\\_\\sde_design\\skin3\\";
+		String fileName = "index.html";
+
+		// 파일 내용 읽기
+		inputCharset = Charset.forName("UTF-8");
+		try (FileInputStream fis = new FileInputStream(filePath + fileName);
+			 InputStreamReader isr = new InputStreamReader(fis, inputCharset);
+			 BufferedReader br = new BufferedReader(isr)) {
+
+			int lineNum = 1;
+			String lineString;
+
+			System.out.println("****** 경로\t : " + filePath);
+			System.out.println("****** 파일명\t : " + fileName);
+			System.out.println("=========================한글 포함 라인===========================================");
+			// read source String each Line
+			while ((lineString = br.readLine()) != null) {
+				// 한글 감지된 라인만 출력
+				if(hd.isContainHangul(lineString)) System.out.println("Line"+ lineNum + "\t" + lineString);
+				lineNum++;
+			}
+			System.out.println("===================== END : " + filePath + fileName + " =================================================");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
--- src/com/malanghoney/ReadHangulInFiles/Hello.java
+++ src/com/malanghoney/ReadHangulInFiles/Hello.java
@@ -1,43 +1,10 @@
 package com.malanghoney.ReadHangulInFiles;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.Charset;
-
 public class Hello {
 
-	private static EncodingChanger ec = new EncodingChanger();
-	private static HangulDetector hd = HangulDetector.getInstance();
-
 	public static void main(String[] args) {
-		Charset inputCharset;
-
-		String filePath = "C:\\dev-Source\\cafe24FTP\\_\\sde_design\\skin3\\";
-		String fileName = "index.html";
-
-		// 파일 내용 읽기
-		inputCharset = Charset.forName("UTF-8");
-		try (FileInputStream fis = new FileInputStream(filePath + fileName);
-			 InputStreamReader isr = new InputStreamReader(fis, inputCharset);
-			 BufferedReader br = new BufferedReader(isr)) {
-
-			int lineNum = 1;
-			String lineString;
-
-			System.out.println("****** 경로\t : " + filePath);
-			System.out.println("****** 파일명\t : " + fileName);
-			System.out.println("=========================한글 포함 라인===========================================");
-			// read source String each Line
-			while ((lineString = br.readLine()) != null) {
-				// 한글 감지된 라인만 출력
-				if(hd.isContainHangul(lineString)) System.out.println("Line"+ lineNum + "\t" + lineString);
-				lineNum++;
-			}
-			System.out.println("===================== END : " + filePath + fileName + " =================================================");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		FileReader fr = new FileReader();
+		fr.printHangulCode();
 	}
 
 }
