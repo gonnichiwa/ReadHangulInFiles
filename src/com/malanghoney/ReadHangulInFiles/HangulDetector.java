a2bdae4fbd34d08dc6c05f24c3c00d2c0ba591bc
jayt.gonnichiwa <jayt.jhjeong@gmail.com>
2018-05-04 15:22:54 +0900

    코드 정리

--- src/com/malanghoney/ReadHangulInFiles/HangulDetector.java
+++ src/com/malanghoney/ReadHangulInFiles/HangulDetector.java
@@ -2,18 +2,19 @@ package com.malanghoney.ReadHangulInFiles;
 
 class HangulDetector {
 
-	private EncodingChanger ec = new EncodingChanger();
-
-	private HangulDetector(){
+	private HangulDetector(){}
+	static HangulDetector getInstance(){
+		return LazyHolder.INSTANCE;
 	}
+	private EncodingChanger ec = new EncodingChanger();
 
 	private static class LazyHolder {
 
 		private static final HangulDetector INSTANCE = new HangulDetector();
 	}
-	static HangulDetector getInstance(){
-		return LazyHolder.INSTANCE;
-	}
+
+	// sourceCode 한줄 왼쪽부터 검색하야
+	// 한글(unicode) 검출시 true
 	boolean isContainHangul(String lineString) {
 		boolean isContain = false;
 		int[] unicodes = ec.getUnicodesFromString(lineString);
@@ -21,7 +22,10 @@ class HangulDetector {
 			/* (UTF-8 INT : UTF-8 HEX )
 			       44032  :   0xAC00
 			       55203  :   0xD7A3 */
-			if(i >= 44032 && i <= 55203) isContain = true;
+			if(i >= 44032 && i <= 55203) {
+				isContain = true;
+				break;
+			}
 		}
 		return isContain;
 	}
--- src/com/malanghoney/ReadHangulInFiles/Hello.java
+++ src/com/malanghoney/ReadHangulInFiles/Hello.java
@@ -25,29 +25,18 @@ public class Hello {
 			int lineNum = 1;
 			String lineString;
 
-//			System.out.println("* 경로\t : " + filePath);
-//			System.out.println("* 파일명\t : " + fileName);
-//			System.out.println("=======한글 포함 라인=======");
+			System.out.println("****** 경로\t : " + filePath);
+			System.out.println("****** 파일명\t : " + fileName);
+			System.out.println("=========================한글 포함 라인===========================================");
 			// read source String each Line
 			while ((lineString = br.readLine()) != null) {
-				int[] codes = ec.getUnicodesFromString(lineString);
-				// TODO : 한글 감지
+				// 한글 감지된 라인만 출력
 				if(hd.isContainHangul(lineString)) System.out.println("Line"+ lineNum + "\t" + lineString);
 				lineNum++;
 			}
+			System.out.println("===================== END : " + filePath + fileName + " =================================================");
 		} catch (Exception e) {
 			e.printStackTrace();
-		}
-	}
-
-	private void test() {
-		/* 이렇게 하는게 아니라 char[] 배열에서 처리해야되나? */
-		String strHan = "    ### 본 디자인의 저작권은 디자인웹굿에 있으며 디자인을 무단 [ 이전/양도/대여/재판매/템플릿제작 ] 은 법적책임을 지게됩니다. ####";
-//		 4자씩 읽어서 유니코드 출력
-		int[] unicodes = ec.getUnicodesFromString(strHan);
-		for(int i : unicodes) {
-			if(i >= 44032 && i <= 55203)
-			System.out.println(i + "\t" + (char)i);
 		}
 	}
 
